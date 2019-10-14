fb9df9ac328de0706c41844a5856c16d80bb0843
大城昂希 <e175766@ie.u-ryukyu.ac.jp>
2019-10-14 15:00:37 +0900

    新しいプロジェクトを開始した

---
+++ EnglishApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  EnglishApp
+//
+//  Created by 大城昂希 on 2019/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EnglishAppTests/EnglishAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  EnglishAppTests.swift
+//  EnglishAppTests
+//
+//  Created by 大城昂希 on 2019/10/14.
+//
+
+import XCTest
+@testable import EnglishApp
+
+class EnglishAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
