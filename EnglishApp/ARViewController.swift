2e0be0b9e4d1c0484bde97cffa356c1407291c12
大城昂希 <e175766@ie.u-ryukyu.ac.jp>
2019-10-21 22:42:04 +0900

    ARSceneViewでカメラを起動できるようになった

---
+++ EnglishApp/ARViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ARViewController.swift
+//  EnglishApp
+//
+//  Created by 大城昂希 on 2019/10/20.
+//
+
+import Foundation
+import UIKit
+import ARKit
+
+class ARViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.scene = SCNScene()
+        sceneView.autoenablesDefaultLighting = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+}
