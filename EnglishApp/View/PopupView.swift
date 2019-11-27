883bca3d9d8f514eb761c42e05dc4d11997b240c
Mayu Koresawa <e175770@ie.u-ryukyu.ac.jp>
2019-11-27 17:37:58 +0900

    報酬画面のpop up windowを表示した

--- EnglishApp/View/PopupView.swift
+++ EnglishApp/View/PopupView.swift
@@ -8,18 +8,23 @@ class PopUpView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = UIColor(rgb: 0x000000)
+        self.backgroundColor = UIColor(rgb: 0xC0C0C0)
         
         self.okButton = {
             let button = UIButton()
-            button.setTitle("Ok", for: .init())
+            button.setTitle("OK", for: .init())
+            button.setTitleColor(.white, for: UIControl.State())
+            button.backgroundColor = UIColor(rgb: 0x78CCD0)
+            button.addTarget(self, action: #selector(tapped(_:)), for: .touchUpInside)
             return button
         }()
         
         self.titleLabel = {
             let label = UILabel()
-            label.text = "保護者の方に渡してね！"
+            label.text = "保護者の方にわたしてね！"
+            //label.backgroundColor = .white
             label.textColor = UIColor(rgb: 0x000000)
+            label.textAlignment = .center
             return label
         }()
         
@@ -35,10 +40,18 @@ class PopUpView: UIView {
         let height = self.frame.height
         let width = self.frame.width
         
-        self.okButton.frame = CGRect(x: 0, y: height*0.8, width: width*0.1, height: height*0.05)
-        self.okButton.center.x = self.center.x
+        self.okButton.frame = CGRect(x: 0, y: height*0.7, width: width*0.2, height: height*0.25)
+        self.okButton.center.x = width*0.5
+        self.okButton.titleLabel?.font = UIFont.systemFont(ofSize: height*0.2)
         
         self.titleLabel.frame = CGRect(x: 0, y: 0, width: width, height: height*0.9)
-        self.titleLabel.center.x = self.center.x
+        self.titleLabel.center.x = width*0.5
+        self.titleLabel.font = UIFont.systemFont(ofSize: height*0.2)
+        
     }
+    
+    @objc func tapped(_ sender: UITapGestureRecognizer){
+        print("tap ok button")
+        self.removeFromSuperview()
+     }
 }
