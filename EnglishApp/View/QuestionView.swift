be44f3a9d777ab1a2e44ff685aa765d05733b027
Ren <e175742@ie.u-ryukyu.ac.jp>
2019-11-27 12:35:44 +0900

    お題画面に音声をつけた

--- EnglishApp/View/QuestionView.swift
+++ EnglishApp/View/QuestionView.swift
@@ -1,14 +1,19 @@
 import Foundation
 import UIKit
+import AVFoundation
 
 class QuestionView: UIView {
     
+    var speechSynthesizer : AVSpeechSynthesizer!
     var questionLabel: UILabel!
     var topLabel: UILabel! //頭文字のラベル
     var searchLabel: UILabel! //"を探そうのラベル"
     
+    
     // 初期化関数
     override init(frame: CGRect) {
+        self.speechSynthesizer = AVSpeechSynthesizer()
+        
         super.init(frame: frame)
         
         self.questionLabel = {
@@ -76,5 +81,52 @@ class QuestionView: UIView {
         
         self.searchLabel.text = "を探してね"
         self.searchLabel.sizeToFit()
+
+        
+        if questionString != nil && questionAlphabet != nil {
+            if questionString!.prefix(1) == questionAlphabet! {
+                let utterance1 = AVSpeechUtterance(string: questionString!) // 読み上げるtext
+                utterance1.voice = AVSpeechSynthesisVoice(language: "en-US") // 言語
+                utterance1.rate = 0.5; // 読み上げ速度
+                utterance1.pitchMultiplier = 1.0; // 読み上げる声のピッチ(1.0でSiri)
+                utterance1.preUtteranceDelay = 0.2; // 読み上げるまでのため
+                self.speechSynthesizer.speak(utterance1)
+                
+                let utterance2 = AVSpeechUtterance(string: "を完成させよう") // 読み上げるtext
+                utterance2.voice = AVSpeechSynthesisVoice(language: "ja-JP") // 言語
+                utterance2.rate = 0.5; // 読み上げ速度
+                utterance2.pitchMultiplier = 1.0; // 読み上げる声のピッチ(1.0でSiri)
+                utterance2.preUtteranceDelay = 0.2; // 読み上げるまでのため
+                self.speechSynthesizer.speak(utterance2)
+                
+                let utterance3 = AVSpeechUtterance(string: questionAlphabet!) // 読み上げるtext
+                utterance3.voice = AVSpeechSynthesisVoice(language: "ja-JP") // 言語
+                utterance3.rate = 0.5; // 読み上げ速度
+                utterance3.pitchMultiplier = 1.0; // 読み上げる声のピッチ(1.0でSiri)
+                utterance3.preUtteranceDelay = 0.2; // 読み上げるまでのため
+                self.speechSynthesizer.speak(utterance3)
+                
+                let utterance4 = AVSpeechUtterance(string: "を探してね") // 読み上げるtext
+                utterance4.voice = AVSpeechSynthesisVoice(language: "ja-JP") // 言語
+                utterance4.rate = 0.5; // 読み上げ速度
+                utterance4.pitchMultiplier = 1.0; // 読み上げる声のピッチ(1.0でSiri)
+                utterance4.preUtteranceDelay = 0.2; // 読み上げるまでのため
+                self.speechSynthesizer.speak(utterance4)
+            } else {
+                let utterance3 = AVSpeechUtterance(string: questionAlphabet!) // 読み上げるtext
+                utterance3.voice = AVSpeechSynthesisVoice(language: "ja-JP") // 言語
+                utterance3.rate = 0.5; // 読み上げ速度
+                utterance3.pitchMultiplier = 1.0; // 読み上げる声のピッチ(1.0でSiri)
+                utterance3.preUtteranceDelay = 0.2; // 読み上げるまでのため
+                self.speechSynthesizer.speak(utterance3)
+                
+                let utterance4 = AVSpeechUtterance(string: "を探してね") // 読み上げるtext
+                utterance4.voice = AVSpeechSynthesisVoice(language: "ja-JP") // 言語
+                utterance4.rate = 0.5; // 読み上げ速度
+                utterance4.pitchMultiplier = 1.0; // 読み上げる声のピッチ(1.0でSiri)
+                utterance4.preUtteranceDelay = 0.2; // 読み上げるまでのため
+                self.speechSynthesizer.speak(utterance4)
+            }
+        }
     }
 }
